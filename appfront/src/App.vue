2a7f005a13b9fec26a63b1d79408b4ab6c6fa02a
Crespo-Vasquez Edison-Enrique <edison.crespo.ext@inetum.com>
2024-12-11 13:40:26 +0100

    Develop: Agregar metodo Autenticar Usuario y realizar llamada

--- appfront/src/App.vue
+++ appfront/src/App.vue
@@ -1,29 +1,55 @@
 <template>
- <form class="formulario">
+  <form class="formulario" >
     <legend>LOGIN</legend>
-    <input type="text" id="" placeholder="username"><br>
-    <input type="password" id="" placeholder="password"><br>
-    <input type="submit" value="Login">
- </form>
+    <input type="text" v-model="email" placeholder="email" /><br />
+    <input type="password" v-model="password" placeholder="password" /><br />
+    <input type="submit" value="Login" @click="authenticateUser"/>
+  </form>
 </template>
 
 <script>
-
+import axios from "axios";
 
 export default {
-  name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: "",
+    };
+  },
+  methods: {
+    authenticateUser() {
+      var json = JSON.stringify({"email":this.email, "password":this.password});
+
+      axios.post("https://localhost:7187/api/Controller/Login",json, {
+        headers: {
+          'Content-Type' : 'application/json'
+        }
+      })
+      .then(function(response){
+     
+        console.log(response)
+      })
+      .catch(function(error){
+       
+        console.log(error)
+      })
+    },
+  },
+};
 </script>
 
 <style>
-  .formulario {
-    display: grid;
-    place-items: center;
-    font-family: monospace;
-    background-color: rgb(158, 232, 232);
-    width: 200px;
-    margin: auto;
-    margin-top: 100px;
-    padding: 10px;
-  }
+.formulario {
+  display: grid;
+  place-items: center;
+  font-family: monospace;
+  background-color: rgb(158, 232, 232);
+  width: 200px;
+  margin: auto;
+  margin-top: 100px;
+  padding: 10px;
+}
 </style>
